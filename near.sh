9e9a30bc3ef4e139cee9ee35a7c003f621019c7a
EasySwap <142835812+EziSwap@users.noreply.github.com>
2024-05-02 01:05:55 +0300

    Update near.sh

--- near.sh
+++ near.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-RPC_URL="https://near.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://near.lava.build/lava-referer-973258f7-14d6-4165-9cef-0de2d69c9455/"
 
 OUTPUT_DIR="logs"
 
-near_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"block","id":"HappyCuanAirdrop","params":{"finality":"final"}}' "$RPC_URL")
+UA=$(shuf -n 1 ua.txt)
+
+axelar_data=$(curl -A UA -x http://104.239.10.201:5872 --proxy-user tihhlvxl:qatvzutcsmo4 -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"status"}' "$RPC_URL")
 
 if [ $? -eq 0 ]; then
     if [ ! -d "$OUTPUT_DIR" ]; then
         mkdir "$OUTPUT_DIR"
     fi
-    
-    echo "$near_data" >> "$OUTPUT_DIR/near.txt"
-    
-    echo "NEAR data fetched and saved to $OUTPUT_DIR/near.txt."
+
+    echo "$axelar_data" >> "$OUTPUT_DIR/axelar.txt"
+
+    echo "Axelar data fetched and saved to $OUTPUT_DIR/axelar.txt."
 else
-    echo "Failed to fetch NEAR data."
+    echo "Failed to fetch Axelar data."
 fi
