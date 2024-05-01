07681d8e4aa8bcac5ca77e5548bc101720b7f79d
EasySwap <142835812+EziSwap@users.noreply.github.com>
2024-05-02 01:06:22 +0300

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-973258f7-14d6-4165-9cef-0de2d69c9455/"
 
 OUTPUT_DIR="logs"
 
-starknet_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"starknet_blockNumber"}' "$RPC_URL")
+UA=$(shuf -n 1 ua.txt)
+
+axelar_data=$(curl -A UA -x http://194.113.119.161:6835 --proxy-user tihhlvxl:qatvzutcsmo4 -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"status"}' "$RPC_URL")
 
 if [ $? -eq 0 ]; then
     if [ ! -d "$OUTPUT_DIR" ]; then
         mkdir "$OUTPUT_DIR"
     fi
-    
-    echo "$starknet_data" >> "$OUTPUT_DIR/starknet.txt"
-    
-    echo "StarkNet data fetched and saved to $OUTPUT_DIR/starknet.txt."
+
+    echo "$axelar_data" >> "$OUTPUT_DIR/axelar.txt"
+
+    echo "Axelar data fetched and saved to $OUTPUT_DIR/axelar.txt."
 else
-    echo "Failed to fetch StarkNet data."
+    echo "Failed to fetch Axelar data."
 fi
